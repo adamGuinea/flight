6bcb233f9a685c77cd017e53a962d76a919dbec5
adamGuinea <adamguinea22@gmail.com>
2019-07-13 17:18:42 +1200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import Moment from "react-moment";
+import jsonData from "./sample_data.json";
+import { ReactComponent as ClockSVG } from "./clock.svg";
+import { ReactComponent as CalendarSVG } from "./calendar.svg";
+import { ReactComponent as PinSVG } from "./pin.svg";
+import { ReactComponent as ToolTipSVG } from "./tooltip.svg";
+import { ReactComponent as StarSVG } from "./star.svg";
+// import { ReactComponent as StarHalfSVG } from "./star-half.svg";
 
-function App() {
+const numberWithCommas = (val) => {
+  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const hidePlaces = (val) => {
+  return val.slice(0, 6).join(", ");
+}
+
+const revealPlaces = (val) => {
+  return val.join(', ')
+}
+
+function render4Stars() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <span>
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+    </span>
   );
 }
+
+function render4AndAHalfStars() {
+  return (
+    <span className='star-half'>
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+      {/* <StarHalfSVG /> */}
+    </span>
+  );
+}
+
+function render5Stars() {
+  return (
+    <span>
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+      <StarSVG />
+    </span>
+  );
+}
+
+const App = () => {
+  const [click, setClick] = useState(false);
+  const toggleClick = () => setClick(!click);
+
+  return (
+    <Fragment>
+      
+      <h2 className='heading'>Select a Tour</h2>
+      <p className='subheading'>
+        <span>3 tours</span> found
+      </p>
+
+      {jsonData.results.map(
+        (
+          {
+            tour_name,
+            image,
+            operator,
+            logo,
+            date_start,
+            date_end,
+            duration,
+            itinerary,
+            price,
+            rating
+          },
+          index
+        ) => (
+          
+          <div className='card' key={index}>
+            <div className='card__img'>
+              <img className='card__img--main' src={image} alt={tour_name} />
+              <img className='card__img--logo' src={operator[0].logo} alt={operator[0].name} />
+            </div>
+            <div className='card__content'>
+              <div className='card__title'>{tour_name}</div>
+              <div className='card__rating'>
+                {rating === 4
+                  ? (render4Stars())
+                  : rating === 4.5
+                  ? render4AndAHalfStars()
+                  : rating === 5
+                  ? render5Stars()
+                  : null}
+              </div>
+              <div>
+                <span className='p-1'>
+                  <CalendarSVG />
+                </span>
+                <Moment format='DD MMM YYYY'>{date_start}</Moment> -{" "}
+                <Moment format='DD MMM YYYY'>{date_end}</Moment>
+              </div>
+
+              <div className='card__content--days'>
+                <span className='p-1'>
+                  <ClockSVG />
+                </span>
+                {""}
+                {duration} days
+              </div>
+
+              <div className='card__content--itinerary'>
+                <span className='p-2'>
+                  <PinSVG />
+                </span>
+                <span className={click ? 'hide' : 'reveal'}>{hidePlaces(itinerary)}...</span>
+                <span className={click ? 'reveal' : 'hide'}>{revealPlaces(itinerary)}</span>
+                <button onClick={toggleClick} className='show-tours'>{click ? '- less' : '+ more'}</button>
+              </div>
+
+              <hr />
+
+              <div className='card__content--info'>
+                <div className='tip'>
+                  <span>from</span>
+                  <ToolTipSVG tabIndex='1' />
+                  <div className='tip__modal'>
+                    <p>Prices are per person and in Australian Dollars</p>
+                  </div>
+                  <div className='price'>
+                    <span>$</span>
+                    {numberWithCommas(price)}
+                  </div>
+                </div>
+                <div>
+                  <button className='card__content--info view'>
+                    view tour
+                  </button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        )
+      )}
+    </Fragment>
+  );
+};
 
 export default App;
